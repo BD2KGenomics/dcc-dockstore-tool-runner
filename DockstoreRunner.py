b49fcddca85366ecdf9cf20508f61ae96146f5bd
Brian O'Connor <briandoconnor@gmail.com>
2016-11-13 14:56:22 -0800

    updates with sample json

--- DockstoreRunner.py
+++ DockstoreRunner.py
@@ -17,7 +17,8 @@ class DockstoreRunner:
         parser.add_argument('--redwood-host', default='storage.ucsc-cgl.org', required=True)
         parser.add_argument('--json-encoded', default='e30=', required=True)
         parser.add_argument('--dockstore-uri', default='quay.io/wshands/fastqc', required=True)
-        parser.add_argument('--parent-uuid', default='parent-UUID-dummy-value', action='append', required=True)
+        parser.add_argument('--parent-uuid', default='parent-UUID-dummy-value', required=True)
+        #parser.add_argument('--parent-uuid', default='parent-UUID-dummy-value', action='append', required=True)
         # get args
         args = parser.parse_args()
         self.redwood_path = args.redwood_path
@@ -32,47 +33,60 @@ class DockstoreRunner:
     def download_and_transform_json(self, json_encoded):
         decoded = base64.urlsafe_b64decode(json_encoded)
         # TODO: so this needs to idenitfy anything with redwood:// and transform it to local path. Also need to deal with output paths
+        parsed_json = json.loads(decoded)
+        print "PARSED JSON: "+decoded
+        for key, value in parsed_json.iteritems():
+            print "ITEM: "+key
+            if isinstance(value, dict):
+                if parsed_json[key]['class'] == 'File':
+                    path = parsed_json[key]['path']
+                    print "PATH: "+path
+            else:
+                for arr_value in parsed_json[key]:
+                    path = arr_value['path']
+                    print "PATH: "+path
+        #for curr_uuid in self.
         # LEFT OFF HERE
         # foreach, download
-        cmd = "java -Djavax.net.ssl.trustStore="+self.redwood_path+"/ssl/cacerts -Djavax.net.ssl.trustStorePassword=changeit -Dmetadata.url="+self.redwood_host+":8444 -Dmetadata.ssl.enabled=true -Dclient.ssl.custom=false -Dstorage.url="+self.redwood_host+":5431 -DaccessToken="+self.redwood_token+" -jar "+self.redwood_path+"/icgc-storage-client-1.0.14-SNAPSHOT/lib/icgc-storage-client.jar download --output-dir ./ --object-id "+self.uuid+" --output-layout bundle"
-        print cmd
-        result = subprocess.call(cmd, shell=True)
-        print "DOWNLOAD RESULT: "+str(result)
-        if result == 0:
-            p = self.output().open('w')
-            print >>p, "finished downloading"
-            p.close()
-        # update JSON structure
-        # write out new JSON file and return path
+        #cmd = "java -Djavax.net.ssl.trustStore="+self.redwood_path+"/ssl/cacerts -Djavax.net.ssl.trustStorePassword=changeit -Dmetadata.url="+self.redwood_host+":8444 -Dmetadata.ssl.enabled=true -Dclient.ssl.custom=false -Dstorage.url="+self.redwood_host+":5431 -DaccessToken="+self.redwood_token+" -jar "+self.redwood_path+"/icgc-storage-client-1.0.14-SNAPSHOT/lib/icgc-storage-client.jar download --output-dir ./ --object-id "+self.uuid+" --output-layout bundle"
+        #print cmd
+#        result = subprocess.call(cmd, shell=True)
+#        print "DOWNLOAD RESULT: "+str(result)
+#        if result == 0:
+#            p = self.output().open('w')
+#            print >>p, "finished downloading"
+#            p.close()
+#        # update JSON structure
+#        # write out new JSON file and return path
 
     def run(self):
         print "** DOWNLOAD **"
         # this will download and create a new JSON
         transformed_json_path = self.download_and_transform_json(self.json_encoded)
 
-        print "** RUN DOCKSTORE TOOL **"
-        cmd = "dockstore tool run --json "+transformed_json_path+" --other args "
-        print cmd
-
-        print "** UPLOAD **"
-        # TODO: need to iterate over the outputs, prepare upload, perform upload, cleanup 
-        cmd = '''mkdir -p %s/%s/upload/%s %s/%s/manifest/%s && ln -s %s/%s/bamstats_report.zip %s/%s/metadata.json %s/%s/upload/%s && \
-echo "Register Uploads:" && \
-java -Djavax.net.ssl.trustStore=%s/ssl/cacerts -Djavax.net.ssl.trustStorePassword=changeit -Dserver.baseUrl=%s:8444 -DaccessToken=`cat %s/accessToken` -jar %s/dcc-metadata-client-0.0.16-SNAPSHOT/lib/dcc-metadata-client.jar -i %s/%s/upload/%s -o %s/%s/manifest/%s -m manifest.txt && \
-echo "Performing Uploads:" && \
-java -Djavax.net.ssl.trustStore=%s/ssl/cacerts -Djavax.net.ssl.trustStorePassword=changeit -Dmetadata.url=%s:8444 -Dmetadata.ssl.enabled=true -Dclient.ssl.custom=false -Dstorage.url=%s:5431 -DaccessToken=`cat %s/accessToken` -jar %s/icgc-storage-client-1.0.14-SNAPSHOT/lib/icgc-storage-client.jar upload --force --manifest %s/%s/manifest/%s/manifest.txt
-''' % (self.tmp_dir, self.bundle_uuid, self.upload_uuid, self.tmp_dir, self.bundle_uuid, self.upload_uuid, self.data_dir, self.bundle_uuid, self.tmp_dir, self.bundle_uuid, self.tmp_dir, self.bundle_uuid, self.upload_uuid, self.ucsc_storage_client_path, self.ucsc_storage_host, self.ucsc_storage_client_path, self.ucsc_storage_client_path, self.tmp_dir, self.bundle_uuid, self.upload_uuid, self.tmp_dir, self.bundle_uuid, self.upload_uuid, self.ucsc_storage_client_path, self.ucsc_storage_host, self.ucsc_storage_host, self.ucsc_storage_client_path, self.ucsc_storage_client_path, self.tmp_dir, self.bundle_uuid, self.upload_uuid)
-        print "CMD: "+cmd
-        result = subprocess.call(cmd, shell=True)
-        if result == 0:
-            cmd = "rm -rf "+self.data_dir+"/"+self.bundle_uuid+"/bamstats_report.zip "+self.data_dir+"/"+self.bundle_uuid+"/datastore/"
-            print "CLEANUP CMD: "+cmd
-            result = subprocess.call(cmd, shell=True)
-            if result == 0:
-                print "CLEANUP SUCCESSFUL"
-            f = self.output().open('w')
-            print >>f, "uploaded"
-            f.close()
+#        print "** RUN DOCKSTORE TOOL **"
+#        cmd = "dockstore tool run --json "+transformed_json_path+" --other args "
+#        print cmd
+#
+#        print "** UPLOAD **"
+#        # TODO: need to iterate over the outputs, prepare upload, perform upload, cleanup
+#        cmd = '''mkdir -p %s/%s/upload/%s %s/%s/manifest/%s && ln -s %s/%s/bamstats_report.zip %s/%s/metadata.json %s/%s/upload/%s && \
+#echo "Register Uploads:" && \
+#java -Djavax.net.ssl.trustStore=%s/ssl/cacerts -Djavax.net.ssl.trustStorePassword=changeit -Dserver.baseUrl=%s:8444 -DaccessToken=`cat %s/accessToken` -jar %s/dcc-metadata-client-0.0.16-SNAPSHOT/lib/dcc-metadata-client.jar -i %s/%s/upload/%s -o %s/%s/manifest/%s -m manifest.txt && \
+#echo "Performing Uploads:" && \
+#java -Djavax.net.ssl.trustStore=%s/ssl/cacerts -Djavax.net.ssl.trustStorePassword=changeit -Dmetadata.url=%s:8444 -Dmetadata.ssl.enabled=true -Dclient.ssl.custom=false -Dstorage.url=%s:5431 -DaccessToken=`cat %s/accessToken` -jar %s/icgc-storage-client-1.0.14-SNAPSHOT/lib/icgc-storage-client.jar upload --force --manifest %s/%s/manifest/%s/manifest.txt
+#''' % (self.tmp_dir, self.bundle_uuid, self.upload_uuid, self.tmp_dir, self.bundle_uuid, self.upload_uuid, self.data_dir, self.bundle_uuid, self.tmp_dir, self.bundle_uuid, self.tmp_dir, self.bundle_uuid, self.upload_uuid, self.ucsc_storage_client_path, self.ucsc_storage_host, self.ucsc_storage_client_path, self.ucsc_storage_client_path, self.tmp_dir, self.bundle_uuid, self.upload_uuid, self.tmp_dir, self.bundle_uuid, self.upload_uuid, self.ucsc_storage_client_path, self.ucsc_storage_host, self.ucsc_storage_host, self.ucsc_storage_client_path, self.ucsc_storage_client_path, self.tmp_dir, self.bundle_uuid, self.upload_uuid)
+#        print "CMD: "+cmd
+#        result = subprocess.call(cmd, shell=True)
+#        if result == 0:
+#            cmd = "rm -rf "+self.data_dir+"/"+self.bundle_uuid+"/bamstats_report.zip "+self.data_dir+"/"+self.bundle_uuid+"/datastore/"
+#            print "CLEANUP CMD: "+cmd
+#            result = subprocess.call(cmd, shell=True)
+#            if result == 0:
+#                print "CLEANUP SUCCESSFUL"
+#            f = self.output().open('w')
+#            print >>f, "uploaded"
+#            f.close()
 
     def output(self):
         return luigi.LocalTarget(self.tmp_dir+"/"+self.bundle_uuid+"/"+self.filename)
--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # dcc-dockstore-tool-runner
 A Dockstore tool designed to perform file downloads from Redwood, run another Dockstore tool, and then upload to Redwood.
+
+## Running Locally
+
+Normally you would not run locally, you are always going to run this via Dockstore or, maybe, via Docker.  For development purposes, though, you may want to setup a local environment for debugging and extending this tool.
+
+## Install Deps
+
+### Ubuntu 14.04
+
+You need to make sure you have system level dependencies installed in the appropriate way for your OS.  For Ubuntu 14.04 you do:
+
+    sudo apt-get install python-dev libxml2-dev libxslt-dev lib32z1-dev
+
+### Python
+
+Use python 2.7.x.
+
+See [here](https://www.dabapps.com/blog/introduction-to-pip-and-virtualenv-python/) for information on setting
+up a virtual environment for Python.
+
+If you haven't already installed pip and virtualenv, depending on your system you may
+(or may not) need to use `sudo` for these:
+
+    sudo easy_install pip
+    sudo pip install virtualenv
+
+Now to setup:
+
+    virtualenv env
+    source env/bin/activate
+    pip install jsonschema jsonmerge openpyxl sets json-spec elasticsearch semver luigi python-dateutil cwl-runner cwltool==1.0.20160316150250 schema-salad==1.7.20160316150109 avro==1.7.7 typing
+
+Alternatively, you may want to use Conda, see [here](http://conda.pydata.org/docs/_downloads/conda-pip-virtualenv-translator.html)
+ [here](http://conda.pydata.org/docs/test-drive.html), and [here](http://kylepurdon.com/blog/using-continuum-analytics-conda-as-a-replacement-for-virtualenv-pyenv-and-more.html)
+ for more information.
+
+    conda create -n schemas-project python=2.7.11
+    source activate schemas-project
+    pip install jsonschema jsonmerge openpyxl sets json-spec elasticsearch semver luigi python-dateutil cwl-runner cwltool==1.0.20160316150250 schema-salad==1.7.20160316150109 avro==1.7.7 typing
+
+### Testing Command
+
+    python DockstoreRunner.py --redwood-path foo --redwood-token token --redwood-host host --json-encoded e30= --dockstore-uri uri --parent-uuid id
+
+    python DockstoreRunner.py --redwood-path foo --redwood-token token --redwood-host host --json-encoded ew0KICAgICJmYXN0cV9maWxlIjogWw0KICAgICAgICB7DQogICAgICAgICJjbGFzcyI6ICJGaWxlIiwNCiAgICAgICAgInBhdGgiOiAicmVkd29vZDovL3N0b3JhZ2UudWNzYy1jZ2wub3JnL2YzOTJmNzljLWE5ZjMtMTFlNi04MGY1LTc2MzA0ZGVjN2ViNy9mMzkzMDBmYy1hOWYzLTExZTYtODBmNS03NjMwNGRlYzdlYjcvTkExMjg3OC1OR3YzLUxBQjEzNjAtQV8xLmZhc3RxLmd6Ig0KICAgICAgICB9LA0KICAgICAgICB7DQogICAgICAgICJjbGFzcyI6ICJGaWxlIiwNCiAgICAgICAgInBhdGgiOiAicmVkd29vZDovL3N0b3JhZ2UudWNzYy1jZ2wub3JnL2YzOTJmNzljLWE5ZjMtMTFlNi04MGY1LTc2MzA0ZGVjN2ViNy9mMzkyZmVhNC1hOWYzLTExZTYtODBmNS03NjMwNGRlYzdlYjcvTkExMjg3OC1OR3YzLUxBQjEzNjAtQV8yLmZhc3RxLmd6Ig0KICAgICAgICB9DQogICAgXQ0KfQ== --dockstore-uri uri --parent-uuid id
+
+This encoded string corresponds to the contents of `sample.json`.
+
+## Via Docker
+
+## Via Dockstore
+
+## Known Issues
+
+    AttributeError: 'str' object has no attribute 'append'
+
+Looks like a bug in Python 2.7 shipped with MacOS since it was fixed on [Ubuntu](https://bugs.launchpad.net/ubuntu/+source/python2.7/+bug/1048710).
